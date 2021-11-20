d33f3d5f1ff930468b3b99c0a203ae11aeab3615
rwormsbecher <Rodney22@live.nl>
2021-11-20 16:32:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ database_name = os.environ.get('database_name', 'fastapi')
 db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
 db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
 ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
+
 DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 
 database = databases.Database(DATABASE_URL)
